e3c1d91e1beb16831902259463517ac6f9898c26
miraclemenikelechi <miracle.emenike7@gmail.com>
2026-04-01 01:18:14 +0100

    feat: add SlidingTokens component for displaying scrolling token information and ConnectWalletButton for wallet connection

---
+++ src/views/top-nav-bar/components/button.tsx
@@ -0,0 +1,6 @@
+import { Button } from "#/components/ui/button.tsx";
+import { cn } from "#/lib/utils.ts";
+
+export function ConnectWalletButton({ className }: { className?: string }) {
+    return <Button className={cn("text-sm", className)}>connect wallet</Button>;
+}

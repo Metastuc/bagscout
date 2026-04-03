7ed3be579e8dc066c31f7427a60367abb3b71c01
miraclemenikelechi <miracle.emenike7@gmail.com>
2026-04-03 14:26:54 +0100

    feat: add error boundary and not-found components with router integration

--- src/router.tsx
+++ src/router.tsx
@@ -2,21 +2,22 @@ import { QueryClient } from "@tanstack/react-query";
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
 
+import { DefaultCatchBoundary } from "./components/layout/error-boundry";
+import { NotFound } from "./components/layout/not-found";
 import { routeTree } from "./routeTree.gen";
 
 export function getRouter() {
     const queryClient = new QueryClient();
     const router = createTanStackRouter({
-        routeTree,
         context: { queryClient },
-        scrollRestoration: true,
+        defaultErrorComponent: DefaultCatchBoundary,
+        defaultNotFoundComponent: () => <NotFound />,
         defaultPreload: "intent",
         defaultPreloadStaleTime: 0,
+        routeTree,
+        scrollRestoration: true,
     });
-    setupRouterSsrQueryIntegration({
-        router,
-        queryClient,
-    });
+    setupRouterSsrQueryIntegration({ queryClient, router });
     return router;
 }
 
